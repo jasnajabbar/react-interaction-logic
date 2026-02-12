77a2cc742bbc69531c0d6c9208e3e0533816beff
jasnajabbar <your-email@example.com>
2026-02-12 14:27:49 +0530

    initial commit

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import Valentine1 from "./Components/Valentine1"
+
+
+function App() {
+
+
+  return (
+    <>
+      <Valentine1/>
+    </>
+  )
+}
+
+export default App
---
+++ src/Components/Valentine1.jsx
@@ -0,0 +1,68 @@
+import React, { useState } from 'react'
+
+const btnStyle={
+      width: "80px", 
+      height:"36px",
+      padding:"5px 10px",
+      border:"0.5px solid #ccc",
+      textAlign:"center"
+  }
+
+function Valentine1() {
+  const [isNoVisible,setIsNoVisible]=useState(true); 
+  const [isNoFirst,setIsNoFirst]=useState(false); 
+  const [msgIndex, setMsgIndex] = useState(-1);
+  // const [message,setMessage]=useState('');
+
+  const messages=["Are you really sure? 🥺",
+  "Last chance… my heart is fragile 💔"];
+
+    const handleNoClick=(()=>{
+      setMsgIndex(prev=>(prev < messages.length - 1 ? prev + 1 : prev));
+
+      if(isNoVisible && !isNoFirst){
+        setIsNoFirst(true)//move before yes
+      }
+      else{
+        setIsNoVisible(false)//vanish completely
+      }
+    })
+  
+  return (
+    <>
+      <div style={{backgroundColor:"#fff0f3",
+        display: "flex",flexDirection: "column",alignItems: "center",
+        justifyContent: "center",height: "300px",border: "1px solid #ddd",
+        borderRadius: "12px",width: "520px",margin: "50px auto",textAlign: "center"}}>
+
+        <h1 style={{margin:"5px 0px"}}><i>Lithuuu ,</i></h1>
+        <h2 style={{margin:"0"}}><i>Will you be my Valentine?😜</i></h2>
+        <h5 style={{margin:"2px 0 0 0"}}><i>(Warning: “No” isn’t an option… the system won’t accept it)</i></h5>
+
+        <div style={{ display: "flex", gap: "10px", marginTop: "10px"}}>
+        {isNoVisible && isNoFirst && (<button onClick={()=>{handleNoClick()}}
+                 style={{...btnStyle,backgroundColor:"#8bca84"}}>No😢</button>)}
+
+          <button onClick={()=>{
+            alert('Yaayyy Thank You Babeee❤️.I have a little surprise for uuu not something I bought, but something from my heart 💕 Come home soon 😊')}} 
+            style={{...btnStyle,backgroundColor:"#FF7F7F"}}>Yes😍</button>
+            
+            {isNoVisible && !isNoFirst && (
+            <button
+              onClick={()=>handleNoClick()}
+              style={{...btnStyle,backgroundColor:"#8bca84"}}
+              >
+              No😢
+            </button>
+        )}        
+         {msgIndex>=0 && (
+          <p style={{color:"red", marginTop:"15px"}}>
+            {messages[msgIndex]}
+          </p>)}
+        </div>
+      </div>
+    </>
+  )
+}
+
+export default Valentine1
